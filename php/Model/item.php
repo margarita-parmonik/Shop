c472596cffd4dd3d9dfafe44f64575d2f9fb7928
Nikita Sugak <sugak.nik@gmail.com>
2022-06-03 16:18:34 +0300

    fix bugs

--- php/Model/item.php
+++ php/Model/item.php
@@ -47,6 +47,19 @@ class Disc extends Item
         echo number_format($this->price, 2, '.', '') . " $" . "<br />";
         echo "Size " . $this->value . " MB <br />";
     }
+
+    public function save_in_bd()
+    {
+        try {
+            $conn = new PDO('mysql:host=localhost', 'root', 'admin');
+        } catch(Exception $e){
+            echo 'Connection failed: ' . $e->getMessage();
+        }
+        
+        $sql = "INSERT INTO shop.items (`sku`, `name`, `price`, `type`, `value`) VALUES ('". $this->sku ."', '". $this->name ."', '". $this->price ."', 'disk', '". $this->value ."')";
+
+        $conn->query($sql);
+    }
 }
 
 class Book extends Item
@@ -57,6 +70,19 @@ class Book extends Item
         echo $this->name . "<br />";
         echo number_format($this->price, 2, '.', '') . " $" . "<br />";
         echo "Weight " . $this->value . " KG <br />";
+    }
+
+    public function save_in_bd()
+    {
+        try {
+            $conn = new PDO('mysql:host=localhost', 'root', 'admin');
+        } catch(Exception $e){
+            echo 'Connection failed: ' . $e->getMessage();
+        }
+        
+        $sql = "INSERT INTO shop.items (`sku`, `name`, `price`, `type`, `value`) VALUES ('". $this->sku ."', '". $this->name ."', '". $this->price ."', 'book', '". $this->value ."')";
+
+        $conn->query($sql);
     }
 }
 
@@ -69,4 +95,19 @@ class Furniture extends Item
         echo number_format($this->price, 2, '.', '') . " $" . "<br />";
         echo "Dimension " .$this->value . "<br />";
     }
+
+    public function save_in_bd()
+    {
+        try {
+            $conn = new PDO('mysql:host=localhost', 'root', 'admin');
+        } catch(Exception $e){
+            echo 'Connection failed: ' . $e->getMessage();
+        }
+        
+        $sql = "INSERT INTO shop.items (`sku`, `name`, `price`, `type`, `value`) VALUES ('". $this->sku ."', '". $this->name ."', '". $this->price ."', 'furniture', '". $this->value ."')";
+
+        $conn->query($sql);
+    }
 }
+// $item = new Furniture(0, "sku", "name", "1.07", "height" );
+//                 $item->save_in_bd();
--- php/save.php
+++ php/save.php
@@ -1,6 +1,38 @@
 <?php
-if(isset($_POST["productType"]))
-{
-    $course = $_POST["productType"];
-    echo $course;
+if (isset($_POST["save"])) {
+    include 'Model/item.php';
+
+    if (isset($_POST["productType"]) && $_POST["productType"] != "") {
+        $productType = $_POST["productType"];
+        $sku = $_POST["sku"];
+        $name = $_POST["name"];
+        $price = $_POST["price"];
+
+        echo $sku;
+        switch ($productType) {
+            case 'furniture':
+                $height = $_POST["height"];
+                $width = $_POST["width"];
+                $length = $_POST["length"];
+
+                $item = new Furniture(0, $sku, $name, $price, $height . 'x' . $width . 'x' . $length);
+                $item->save_in_bd();
+                break;
+            case 'disk':
+                $size = $_POST["size"];
+                
+                $item = new Disc(0, $sku, $name, $price, $size);
+            
+                $item->save_in_bd();
+                break;
+            case 'book':
+                $weight = $_POST["weight"];
+                $item = new Book(0, $sku, $name, $price, $weight);
+                
+                print_r($item);
+                $item->save_in_bd();
+                break;
+        }
+    }
 }
+header('Location: ' . '..');
